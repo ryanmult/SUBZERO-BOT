5700afecba8a15ff16aea8e422bc27f1703c155d
ryanmult <166868260+ryanmult@users.noreply.github.com>
2025-02-03 15:30:46 +0300

    Elf

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID " SUBZERO-MD~3mRUESRa#WYRCYnmr-x0AJ4_xCjo7KG5CIxOtY45v_S2Vhzy6qlw",
+SESSION_ID: process.env.SESSION_ID "SUBZERO-MD~3mRUESRa#WYRCYnmr-x0AJ4_xCjo7KG5CIxOtY45v_S2Vhzy6qlw ",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
